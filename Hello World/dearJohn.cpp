075a32abadcddeb89fd5efee12b2bf1de1a650af
GPV <gvill005@gmail.com>
2024-06-19 00:39:54 -0700

    worked first self writting program in c++ with no notes

---
+++ Hello World/dearJohn.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Dear John," << endl << endl;
+    cout << "I am afraid that we must part." << endl << endl;
+    cout << "Mary" << endl;
+
+    return 0;
+}
+
