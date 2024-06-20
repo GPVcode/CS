3ccac51203e48ebbe43784d3076ba8cfc17c6145
GPV <gvill005@gmail.com>
2024-06-20 10:30:27 -0700

    testing myself

--- Hello World/dearJohn.cpp
+++ Hello World/dearJohn.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 int main(){
-
-    cout << "Dear John," << endl << endl;
-    cout << "I am afraid that we must part." << endl << endl;
-    cout << "Mary" << endl;
+    cout << "Dear John" << endl << endl << "You know how you really like your mug?" << endl;
+    cout << endl;
+    cout << "Sorry." << endl;
+    cout << endl;
+    cout << "Samantha" << endl;
 
     return 0;
 }
-
