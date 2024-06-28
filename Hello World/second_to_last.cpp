141ea33501ab1fbba3173635f77ac0a2946db8f1
GPV <gvill005@gmail.com>
2024-06-28 16:53:18 -0700

    learned a really cool way to use mod and arithmetic to extract specific values from a number

---
+++ Hello World/second_to_last.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+// THis is how to extract the second to last number
+int main(){
+
+int number = 501;
+int second_to_last;
+
+second_to_last = (number / 10) % 10;
+
+cout << "The second to last number is " << second_to_last << endl;
+
+return 0;
+}
