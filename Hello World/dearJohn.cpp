f167db477ac9337ae8720aa78d38b7c62a8daeb8
GPV <gvill005@gmail.com>
2024-07-05 23:23:21 -0700

    working on stuff

--- Hello World/dearJohn.cpp
+++ Hello World/dearJohn.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+// I am officially an embedded systems engineer.
+
 int main(){
     cout << "Dear John" << endl << endl << "You know how you really like your mug?" << endl;
     cout << endl;
