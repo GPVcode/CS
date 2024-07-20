2bb80100a591004ba03a2158a5a45ec4f5d005b9
GPV <gvill005@gmail.com>
2024-07-20 16:13:27 -0700

    working on comp sci final project

--- Hello World/comp-sci-group-project.cpp
+++ Hello World/comp-sci-group-project.cpp
@@ -1,28 +1,85 @@
-// C++ program to illustrate while loop 
+#include <iostream>
 
-#include <iostream> 
-using namespace std; 
+using namespace std;
 
-// input questions
-	// how many sides for printing?
-	// how many print locations?
-	// how many shirts?
-	// how many colors?
-	// sizes?
-// make a function that calculates the total
+// Create three classes
+	// 1. Order class - this class takes the input from customer questions and sends it to order calculator class
+	// 2. Pricing class - this class holds data on pricing structure.
+	// 3. Order calculator class - this class takes the input information from order class and the pricing detailes from pricing class and calculates the total.
 
-int main() 
-{ 
-	// initialization expression 
-	int i = 1; 
+// first let's get basic pricing to work.
+	// establish a shirt cost for 10 dollars and get the total cost to output properly
 
-	// test expression 
-	while (i < 6) { 
-		cout << "Hello World\n"; 
 
-		// update expression 
-		i++; 
-	} 
+// create pricing class
+// create order class
+// create order calculator class
+main(){
 
-	return 0; 
-}
+	double totalCost = 0.0;
+
+	class ShirtOrder {
+		public: 
+			bool printBothSides;
+			int colorCount;
+			int shirtCount;
+			int small, medium, large, xl;
+
+			void printOnBothSides(string n){
+				if(n == "yes"){
+					printBothSides = true;
+				} else {
+					printBothSides = false;
+				};
+			};
+			void shirtColorCount(int n){
+				colorCount = n;
+			};
+			void shirtCount(int n){
+				shirtCount = n;
+			};
+			void smallCount(int n){
+				small = n;
+			};
+			void mediumCount(int n){
+				medium = n;
+			};
+			void largeCount(int n){
+				large = n;
+			};
+			void xlCount(int n){
+				xl = n;
+			};
+	};
+
+
+	class CostCalculator(){
+
+	};
+
+	do{
+		ShirtOrder
+
+		// Print on both sides?
+		cout << "Do you want to print on both sides?";
+		// How many colors?
+		// How many t-shirts to print?
+		// Out of X shirts, type in how many small (if no more shirts left after the input and calculation, break the loop)
+		// (Subtract from total shirts) out of X(updated shirt count) shirts, type how many medium( if no more shirts left after the input and calculation, break the loop)
+		// (Subtract from total shirts) out of X(updated shirt count) shirts, type how many large shirts (if no more shirts left after the input and calculation, break the loop)
+		// Subtract from total shirts) you want X(updates shirt counts) XL shirts correct?
+
+		totalCost = // output from 
+		// get info from order class
+		// get info from pricing class
+		// run order calculator
+
+	} while(totalCost == 0.0); {
+
+
+		cout << "Total cost of the order is: $ " << totalCost << endl;
+
+	};
+
+	return 0;
+};
